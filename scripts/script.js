fe34df492dd510a81a91695c5f950c17e6fe5bb1
Lois Simpson <lsimpson667@gmail.com>
2020-03-23 16:37:32 -0400

    3rd commit: fixed the 'cancelled' option for userName entry and also for the 'txt' entry in the 'addNewPothole function

--- scripts/script.js
+++ scripts/script.js
@@ -9,25 +9,27 @@ function showUsernameMessage() {
             } else {
                 alert(`Wrong password.`);
             }
+    } else if (userName === "" || userName === undefined) {
+        alert(`Cancelled`);
     } else {
         alert(`Your username does not match the profiles in our database.`);
-    } 
+    }
     
 }
 
 let password;
-// Ask Kiki or Melissa questions about why the Line Break doesnt workVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
 
-let txt2 = `Thanks! I'll add that to the list`;
+let txt;
+let txt2 = `Thanks! I'll add that to the list.`;
 function addNewPothole() {
     
     let newPothole = prompt(`What is the name of your new pothole?`);
 
-    let txt;
-    txt2 += `<p>${newPothole}<p>`;
+    txt = `<p>Cancelled</p>`;
+    txt2 += `<p>${newPothole}</p>`;
 
     if (newPothole === "" || newPothole === undefined || newPothole === null) {
-        txt = "Cancelled";
+        document.getElementById("addNewPothole").innerHTML = txt;
     } else {
         document.getElementById("addNewPothole").innerHTML = txt2;
     }
